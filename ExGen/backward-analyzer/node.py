1977294c3763a30be5f24457629d8656893c56f6
Mojtaba Eshghie <eshghie@kth.se>
2024-09-04 11:03:59 +0200

    Enhancement #3: Removing unncessary directories

--- ExGen/backward-analyzer/node.py
+++
@@ -1,5 +0,0 @@
-class Node(object):
-    def __init__(self, func, name, attr):
-        self.func = func
-        self.name = name
-        self.attr = attr  # record taint flag
--- teether/build/lib/teether/cfg/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import bb
-from . import cfg
-from . import disassembly
-from . import instruction
-from . import opcodes
